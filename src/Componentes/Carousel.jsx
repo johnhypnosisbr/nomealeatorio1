13c9b52c57f62104ee87a66aef3d394cf5f2e0f2
johnhypnosisbr <john.kmick@yahoo.com.br>
2023-07-25 21:18:11 -0300

    resolvi o banner

--- src/Componentes/Carousel.jsx
+++ src/Componentes/Carousel.jsx
@@ -9,17 +9,19 @@ function ControlledCarousel() {
   };
 
   return (
-    <Carousel fade activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img src="../img/banner1.jpg" className="carousel"/>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="../img/banner2.jpg" className="carousel"/>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img src="../img/banner3.jpg" className="carousel"/>
-      </Carousel.Item>
-    </Carousel>
+    <div className="centro">
+      <Carousel fade activeIndex={index} sm={12} md={12} lg={12} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img src="../img/banner1.jpg" className="carousel" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="../img/banner2.jpg" className="carousel" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img src="../img/banner3.jpg" className="carousel" />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   );
 }
 

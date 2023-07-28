489ff1852a22aff7caf560efb573636a72dd0868
johnhypnosisbr <john.kmick@yahoo.com.br>
2023-07-27 22:38:10 -0300

    atualizamos rotas

--- src/Componentes/Navbar.jsx
+++ src/Componentes/Navbar.jsx
@@ -1,18 +1,37 @@
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function CompNavbar() {
+  const [name, setName] = useState('NOME')
+  const [email, setEmail] = useState('EMAIL')
+
+  function pegarNomeEmailDoLocalStorage() {
+    const emailLocalStorage = localStorage.getItem("emailLoggedUser")
+    const nameLocalStorage = localStorage.getItem("nameLoggedUser")
+
+    setEmail(emailLocalStorage)
+    setName(nameLocalStorage)
+  }
+
+  useEffect(() => {
+    pegarNomeEmailDoLocalStorage()
+  }, [])
+
   return (
     <Navbar bg="primary" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Lojinha do John</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/produto">Produtos</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <Container>
+        <Navbar.Brand href="#home">Lojinha do John</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/home">Home</Nav.Link>
+          <Nav.Link href="/produto">Produtos</Nav.Link>
+          <p>
+            {name} | {email}
+          </p>
+        </Nav>
+      </Container>
+    </Navbar>
   )
 }
 
